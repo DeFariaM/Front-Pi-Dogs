d1256a3a59ea083dcfff572c12a813fefb839ef6
MDeFaria <maria.defariasf@gmail.com>
2023-11-03 23:00:35 -0400

    Paginado

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import Card from "../Card/Card";
+import { useState } from "react";
 
-const Cards = ({ allUsers }) => {
-  const usersCard = allUsers;
+const Cards = ({ dogs }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8;
+
+  const prev = () => {
+    const prevPage = currentPage - 1;
+    if (prevPage <= 0) return;
+    setCurrentPage(prevPage);
+  };
+
+  const next = () => {
+    const total = dogs.length;
+    const nextPage = currentPage + 1;
+    const firstIndex = nextPage * itemsPerPage;
+    if (firstIndex >= total) return;
+
+    setCurrentPage(nextPage);
+  };
 
   return (
     <div>
-      {usersCard.map((user) => {
-        return <Card key={user.id} user={user} />;
-      })}
+      <button onClick={prev}>Prev</button>
+      <button onClick={next}>Next</button>
+
+      {dogs
+        .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+        .map((dog, index) => (
+          <Card key={index} dog={dog} />
+        ))}
     </div>
   );
 };
