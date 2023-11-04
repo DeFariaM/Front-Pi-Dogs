d40abcab2324e2ce0e08c084e4afc9bb5f6864ca
MDeFaria <maria.defariasf@gmail.com>
2023-11-03 23:01:19 -0400

    Detail

--- src/Views/Detail.jsx
+++ src/Views/Detail.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { clearData, getDogsID } from "../Redux/Actions/actions";
 
 const Detail = () => {
-  return <div>Detail</div>;
+  const { id } = useParams();
+
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.detailDog);
+  const dog = data[0];
+
+  useEffect(() => {
+    dispatch(getDogsID(id));
+    return () => dispatch(clearData());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>{dog?.name}</h1>
+    </div>
+  );
 };
 
 export default Detail;
