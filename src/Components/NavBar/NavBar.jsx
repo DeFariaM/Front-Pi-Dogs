462e8bf6ce06ed68f5d6cd4497f3d085f5071aa7
MDeFaria <maria.defariasf@gmail.com>
2023-11-03 23:01:09 -0400

    Buttons

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import { NavLink, useLocation } from "react-router-dom";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const { pathname } = useLocation();
+  return (
+    <div>
+      {pathname === "/home" ? <SearchBar /> : null}
+      {pathname !== "/create" ? (
+        <NavLink to="/create">
+          <button>Create your own dog</button>
+        </NavLink>
+      ) : null}
+      {pathname !== "/home" ? (
+        <NavLink to="/home">
+          <button>Home</button>
+        </NavLink>
+      ) : null}
+    </div>
+  );
 };
 
 export default NavBar;
