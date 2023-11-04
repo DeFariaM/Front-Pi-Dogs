dab00f43dff9d9f8eb4e24936e76d24b3476dbe2
MDeFaria <maria.defariasf@gmail.com>
2023-11-03 23:01:35 -0400

    Connection to state

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,21 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getUsers } from "../Redux/Actions/actions";
+import { getDogs } from "../Redux/Actions/actions";
 import Cards from "../Components/Cards/Cards";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const allUsers = useSelector((state) => state?.allUsers);
+  const allDogs = useSelector((state) => state?.allDogs);
 
   useEffect(() => {
-    dispatch(getUsers());
+    dispatch(getDogs());
   }, [dispatch]);
 
   return (
     <div>
-      <Cards allUsers={allUsers} />
+      <Cards dogs={allDogs} />
     </div>
   );
 };
+/* {countries
+  .slice(
+    (currentPage - 1) * countriesPerPage,
+    currentPage * countriesPerPage
+  )
+  .map((country) => (
+    <Card
+      key={country.ID}
+      id={country.ID}
+      name={country.name}
+      flags={country.flags}
+      continent={country.continent}
+    />
+  ))}
+ */
 
 export default Home;
