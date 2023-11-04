0930f3e2ca1bf17c355a45e2bb492f5bc1dd09ec
MDeFaria <maria.defariasf@gmail.com>
2023-11-03 22:59:48 -0400

    Adding navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Landing from "./Views/Landing";
 import Home from "./Views/Home";
 import Detail from "./Views/Detail";
 import Create from "./Views/Create";
 import "./App.css";
+import NavBar from "./Components/NavBar/NavBar";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { pathname } = useLocation();
   return (
     <>
       <div>
+        {pathname !== "/" ? <NavBar /> : null}
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/home" element={<Home />}></Route>
