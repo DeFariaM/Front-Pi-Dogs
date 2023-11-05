b0e9d5c38800f623e3adeed03b9084ee2b90df16
MDeFaria <maria.defariasf@gmail.com>
2023-11-04 22:28:21 -0400

    Filtrado y ordenamiento funcionando

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,36 +1,106 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getDogs } from "../Redux/Actions/actions";
+import {
+  clearOrders,
+  filterByOrigin,
+  filterByTemperaments,
+  getDogs,
+  getTemperaments,
+  orderByName,
+  orderByWeight,
+} from "../Redux/Actions/actions";
 import Cards from "../Components/Cards/Cards";
 
 const Home = () => {
   const dispatch = useDispatch();
   const allDogs = useSelector((state) => state?.allDogs);
+  const temperaments = useSelector((state) => state.temperaments);
+  const [aux, setAux] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     dispatch(getDogs());
+    dispatch(getTemperaments());
+    return () => dispatch(clearOrders());
   }, [dispatch]);
+
+  const handleOrder = (e) => {
+    dispatch(orderByName(e.target.value));
+    setAux(!aux);
+    setCurrentPage(1);
+  };
+  const handleOrderWeight = (e) => {
+    dispatch(orderByWeight(e.target.value));
+    setAux(!aux);
+    setCurrentPage(1);
+  };
+
+  const handleFilterTemp = (e) => {
+    e.preventDefault();
+    dispatch(filterByTemperaments(e.target.value));
+    setCurrentPage(1);
+  };
+
+  const handleFilterOrigin = (e) => {
+    e.preventDefault();
+    dispatch(filterByOrigin(e.target.value));
+    setCurrentPage(1);
+  };
+
+  const reset = () => {
+    dispatch(getDogs());
+  };
 
   return (
     <div>
-      <Cards dogs={allDogs} />
+      <div>
+        <select
+          name="temperaments"
+          id="temps"
+          defaultValue={"placeholder"}
+          onChange={handleFilterTemp}>
+          <option name={"placeholder"} hidden>
+            Temperaments
+          </option>
+          {temperaments?.map((t) => {
+            return (
+              <option key={t.id} value={t.name}>
+                {t.name}
+              </option>
+            );
+          })}
+        </select>
+
+        <select defaultValue={"placeholder"} onChange={handleFilterOrigin}>
+          <option name={"placeholder"} hidden>
+            Origin
+          </option>
+          <option value="DB">Created</option>
+          <option value="API">Existent</option>
+          <option value="ALL">All</option>
+        </select>
+      </div>
+      <button onClick={reset}>reset</button>
+      <button value={"ASC"} onClick={handleOrder}>
+        A - Z
+      </button>
+      <button value={"DESC"} onClick={handleOrder}>
+        Z - A
+      </button>
+      <button value={"Lighter"} onClick={handleOrderWeight}>
+        Lighter
+      </button>
+      <button value={"Heavier"} onClick={handleOrderWeight}>
+        Heavier
+      </button>
+
+      <Cards
+        dogs={allDogs}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 };
-/* {countries
-  .slice(
-    (currentPage - 1) * countriesPerPage,
-    currentPage * countriesPerPage
-  )
-  .map((country) => (
-    <Card
-      key={country.ID}
-      id={country.ID}
-      name={country.name}
-      flags={country.flags}
-      continent={country.continent}
-    />
-  ))}
- */
 
 export default Home;
