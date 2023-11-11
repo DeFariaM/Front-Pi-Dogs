aa2b2e3b1b6dd9d62dd5451ae63c40f5758c0d2f
MDeFaria <maria.defariasf@gmail.com>
2023-11-10 22:00:33 -0400

    Card style

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Card.css";
 
 const Card = ({ dog }) => {
+  const temp = dog.Temperaments.slice(0, 2).join(", ");
+
   return (
-    <div>
-      <h1>{dog?.name}</h1>
+    <div className="wrapper">
+      <NavLink to={`/detail/${dog.id}`} className="link">
+        <h3 className="holi">{dog?.name}</h3>
+        <div className="wrap-img">
+          <img src={dog.image} alt="" />
+        </div>
+        <p>
+          Temperaments <br />
+          {temp}
+        </p>
+        <h4>
+          Height Min: {dog.weight_min} Max: {dog.weight_max}
+        </h4>
+      </NavLink>
     </div>
   );
 };
