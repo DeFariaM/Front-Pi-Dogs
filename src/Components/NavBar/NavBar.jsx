ad4137c40fb9f753e3254da2c20606e19bae071a
MDeFaria <maria.defariasf@gmail.com>
2023-11-10 22:01:11 -0400

    Nav Bar style

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import { NavLink, useLocation } from "react-router-dom";
+import style from "./NavBar.module.css";
+const { wrapper, btn, link, logo, name } = style;
+import origami from "../../assets/dog-origami.svg";
 
 const NavBar = ({ setCurrentPage }) => {
   const { pathname } = useLocation();
   return (
-    <div>
+    <div className={wrapper}>
+      <img src={origami} alt="" className={logo} />
+      <span className={name}>DOGKAHOLIC</span>
       {pathname === "/home" ? (
         <SearchBar setCurrentPage={setCurrentPage} />
       ) : null}
       {pathname !== "/create" ? (
-        <NavLink to="/create">
-          <button>Create your own dog</button>
+        <NavLink to="/create" className={link}>
+          <button className={btn}>Create your own dog</button>
         </NavLink>
       ) : null}
       {pathname !== "/home" ? (
-        <NavLink to="/home">
-          <button>Home</button>
+        <NavLink to="/home" className={link}>
+          <button className={btn}>Home</button>
         </NavLink>
       ) : null}
     </div>
