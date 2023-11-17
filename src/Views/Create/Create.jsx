9ff8966e5cba42d21f885f018cd715cc58a52cdd
MDeFaria <maria.defariasf@gmail.com>
2023-11-17 01:03:56 -0400

    Create ready

---
+++ src/Views/Create/Create.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "../../Components/Form/Form";
+
+const Create = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};
+
+export default Create;
