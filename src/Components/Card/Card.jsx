5c21d21edc0dea50c30ff54579efe0edcfd951f3
MDeFaria <maria.defariasf@gmail.com>
2023-11-17 01:00:39 -0400

    Details on card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import "./Card.css";
+import style from "./Card.module.css";
+const { divTemp, wrapper, dog_name, wrap_img, img_dog, link, tempSpan } = style;
 
 const Card = ({ dog }) => {
-  const temp = dog.Temperaments.slice(0, 2).join(", ");
+  const temp = dog.Temperaments.slice(0, 2).join(", ").split(", ");
 
   return (
-    <div className="wrapper">
-      <NavLink to={`/detail/${dog.id}`} className="link">
-        <h3 className="holi">{dog?.name}</h3>
-        <div className="wrap-img">
-          <img src={dog.image} alt="" />
+    <div className={wrapper}>
+      <NavLink to={`/detail/${dog.id}`} className={link}>
+        <h3 className={dog_name}>{dog?.name}</h3>
+        <div className={wrap_img}>
+          <img src={dog.image} alt="" className={img_dog} />
         </div>
-        <p>
-          Temperaments <br />
-          {temp}
-        </p>
+        <div className={divTemp}>
+          {temp?.map((t, index) => {
+            return (
+              <p className={tempSpan} key={index}>
+                {t}
+              </p>
+            );
+          })}
+        </div>
+
         <h4>
-          Height Min: {dog.weight_min} Max: {dog.weight_max}
+          Weight Min: {dog.weight_min ? ` ${dog.weight_min} kg ` : 0} Max:{" "}
+          {dog.weight_max ? ` ${dog.weight_max} kg ` : 0}
         </h4>
       </NavLink>
     </div>
