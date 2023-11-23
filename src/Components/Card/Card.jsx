b4aba6907dcbdf76df1dd02fdbd8437bfdf2c8fe
MDeFaria <maria.defariasf@gmail.com>
2023-11-23 14:20:24 -0400

    Card fixed

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,36 +1,43 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import style from "./Card.module.css";
-const { divTemp, wrapper, dog_name, wrap_img, img_dog, link, tempSpan } = style;
+const {
+  divTemp,
+  wrapper,
+  dog_name,
+  wrap_img,
+  img_dog,
+  link,
+  tempSpan,
+  overlay,
+} = style;
 
 const Card = ({ dog }) => {
-  const temp = dog.Temperaments.slice(0, 2).join(", ").split(", ");
+  const temp = dog.Temperaments.slice(0, 3).join(", ").split(", ");
 
   return (
     <div className={wrapper}>
+      <h3 className={dog_name}>{dog?.name}</h3>
+      <div className={wrap_img}>
+        <img src={dog.image} alt="" className={img_dog} />
+      </div>
       <NavLink to={`/detail/${dog.id}`} className={link}>
-        <h3 className={dog_name}>{dog?.name}</h3>
-        <div className={wrap_img}>
-          <img src={dog.image} alt="" className={img_dog} />
-        </div>
-        <div className={divTemp}>
-          {temp.length > 0 ? (
-            temp.map((t, index) => {
+        <div className={overlay}>
+          <div className={divTemp}>
+            {temp?.map((t, index) => {
               return (
                 <p className={tempSpan} key={index}>
                   {t}
                 </p>
               );
-            })
-          ) : (
-            <p className={tempSpan}>No registered temperaments</p>
-          )}
-        </div>
+            })}
+          </div>
 
-        <h4>
-          Weight Min: {dog.weight_min ? ` ${dog.weight_min} kg ` : 0} Max:{" "}
-          {dog.weight_max ? ` ${dog.weight_max} kg ` : 0}
-        </h4>
+          <h4>
+            Weight Min: {dog.weight_min ? ` ${dog.weight_min} kg ` : 0} Max:{" "}
+            {dog.weight_max ? ` ${dog.weight_max} kg ` : 0}
+          </h4>
+        </div>
       </NavLink>
     </div>
   );
